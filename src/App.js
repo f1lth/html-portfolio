87f589e047a00824ece1e8202fa2fda718bd915e
f1lth <maxwell.kalifornia@gmail.com>
2023-07-24 16:19:27 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>f1lth</h2>
+      <p><a href="https://github.com">GitHub</a></p>
+      <div className='content'>
+        <h2>Work</h2>
+        
+        <ul> 
+
+          <li><a href="https://icaps23.icaps-conference.org/program/tutorials/model/">LOCM: </a> model acquisition algorithm implementation, presented at ICAPS23' Prague</li>
+          <li><a href="https://github.com/AI-Planning/macq"> macQ:  </a> open source model acquisition library</li>
+          <li><a href="https://supercart.ai/">supercart: </a>  built beta backend for ERC compliant swaps</li>
+          <li><a href="https://aupl6-yaaaa-aaaak-ae3eq-cai.icp0.io/">ckBTC POS: </a>  internet computer dapp to manage payments and transactions for BNT-6</li>
+          
+        </ul>
+        </div>
+        <div className='content'>
+          <h2>Education</h2>
+          <ul>
+            <li>Queen's University - B.Comp (AI) - Ongoing</li>
+          </ul>
+        </div>
     </div>
   );
 }

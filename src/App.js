e79279711fa2ed08324df231e132484330b97529
f1lth <maxwell.kalifornia@gmail.com>
2023-07-25 15:02:43 -0400

    changed links

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           <li><a href="https://github.com/AI-Planning/macq"> macQ:  </a> open source model acquisition library</li>
           <li><a href="https://supercart.ai/">supercart: </a>  built beta backend for ERC compliant swaps</li>
           <li><a href="https://aupl6-yaaaa-aaaak-ae3eq-cai.icp0.io/">ckBTC POS: </a>  internet computer dapp to manage payments and transactions for BNT-6</li>
-          <li><a href="https://aupl6-yaaaa-aaaak-ae3eq-cai.icp0.io/">raman spectroscopy blood glucose analysis: </a>  non invasive blood glucose readings from raman spectroscopy </li>
+          <li><a href="https://github.com/f1lth/non-invasive-diabetes-classification/">raman spectroscopy blood glucose analysis: </a>  non invasive blood glucose readings from raman spectroscopy </li>
           
         </ul>
         </div>

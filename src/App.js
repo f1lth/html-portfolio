761db61bb4ae65a7541993aa51895906b421385a
f1lth <maxwell.kalifornia@gmail.com>
2023-07-25 13:48:57 -0400

    perhaps final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ function App() {
   return (
     <div className='App'>
       <h2>f1lth</h2>
-      <p><a href="https://github.com">GitHub</a></p>
+      <h3><a href="https://github.com">GitHub</a></h3>
+      
+      <p> Software Eng. currently interested in artificial intelligence, trading, and blockchain. Working at a Minerva -- health startup</p>
+      
       <div className='content'>
         <h2>Work</h2>
         
@@ -15,6 +18,7 @@ function App() {
           <li><a href="https://github.com/AI-Planning/macq"> macQ:  </a> open source model acquisition library</li>
           <li><a href="https://supercart.ai/">supercart: </a>  built beta backend for ERC compliant swaps</li>
           <li><a href="https://aupl6-yaaaa-aaaak-ae3eq-cai.icp0.io/">ckBTC POS: </a>  internet computer dapp to manage payments and transactions for BNT-6</li>
+          <li><a href="https://aupl6-yaaaa-aaaak-ae3eq-cai.icp0.io/">raman spectroscopy blood glucose analysis: </a>  non invasive blood glucose readings from raman spectroscopy </li>
           
         </ul>
         </div>
@@ -22,6 +26,12 @@ function App() {
           <h2>Education</h2>
           <ul>
             <li>Queen's University - B.Comp (AI) - Ongoing</li>
+          </ul>
+        </div>
+        <div className='content'>
+          <h2>Contact</h2>
+          <ul>
+            <li>18mjbk@queensu.ca</li>
           </ul>
         </div>
     </div>

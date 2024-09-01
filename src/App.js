3c5cb019a4a27b32b92ed48d60542bc14cfdd5ff
f1lth <maxwell.kalifornia@gmail.com>
2024-09-01 16:42:02 -0400

    update

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <h2>f1lth</h2>
       <h3><a href="https://github.com/f1lth">GitHub</a></h3>
       
-      <p> Software Eng. currently interested in artificial intelligence, trading, and blockchain. Working at Minerva-- my startup</p>
+      <p> Software Eng. currently interested in artificial intelligence, trading.</p>
       
       <div className='content'>
         <h2>Work</h2>
         
         <ul> 
-
+        <li> Astrace LLC & Minerva, failed startup attempting to tie in LLM and AI data comprehension to simplify doctors lives-- prognosis: Too many regulations</li>
           <li><a href="https://icaps23.icaps-conference.org/program/tutorials/model/">LOCM: </a> model acquisition algorithm implementation, demo'd at ICAPS23' Prague</li>
-          <li><a href="https://github.com/AI-Planning/macq"> macQ:  </a> open source model acquisition library</li>
+          <li><a href="https://github.com/AI-Planning/macq"> macQ:  </a> from locm open source model acquisition library</li>
           <li><a href="https://supercart.ai/">supercart: </a>  built beta backend for ERC compliant swaps</li>
           <li><a href="https://aupl6-yaaaa-aaaak-ae3eq-cai.icp0.io/">ckBTC POS: </a>  internet computer dapp to manage payments and transactions for BNT-6</li>
           <li><a href="https://github.com/f1lth/non-invasive-diabetes-classification/">raman spectroscopy blood glucose analysis: </a>  non invasive blood glucose readings from raman spectroscopy </li>
@@ -25,7 +25,7 @@ function App() {
         <div className='content'>
           <h2>Education </h2>
           <ul>
-            <li>Queen's University - B.Comp (AI) - Ongoing</li>
+            <li>Queen's University, Kingston, Ontario - Bsc. Computer Science</li>
           </ul>
         </div>
         <div className='content'>
